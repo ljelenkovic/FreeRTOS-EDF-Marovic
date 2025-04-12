af2000f642e873d3ee6e957eec81890cdb3cbafa
LukasMarovic <lukas.marovic@fer.unizg.hr>
2025-04-12 17:04:26 +0200

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,16 +3,16 @@
 #include "driver/gpio.h"
 #include "esp_system.h"
 
-#define BUTTON_PIN GPIO_NUM_26
+// #define BUTTON_PIN GPIO_NUM_26
 
-static uint32_t last_interrupt_time = 0;
-
-void IRAM_ATTR isr_handler(void *arg) {
-    uint32_t current_time = xTaskGetTickCountFromISR();
-    if ((current_time - last_interrupt_time) > pdMS_TO_TICKS(200)) {
-    }
-    last_interrupt_time = current_time;
-}
+// static uint32_t last_interrupt_time = 0;
+// 
+// void IRAM_ATTR isr_handler(void *arg) {
+//     uint32_t current_time = xTaskGetTickCountFromISR();
+//     if ((current_time - last_interrupt_time) > pdMS_TO_TICKS(200)) {
+//     }
+//     last_interrupt_time = current_time;
+// }
 
 void test(void *pvParameter) {
     int n = *((int *) pvParameter);
@@ -22,25 +22,25 @@ void test(void *pvParameter) {
     // printf("END WORK\n");
 }
 
-void button_task(void *pvParameter)
-{
-    while (1) 
-    {
-        if (gpio_get_level(BUTTON_PIN)) {
-            edf_start_scheduler();
-            vTaskDelay(pdMS_TO_TICKS(1000));
-        }
-    }
-}
+// void button_task(void *pvParameter)
+// {
+//     while (1) 
+//     {
+//         if (gpio_get_level(BUTTON_PIN)) {
+//             edf_start_scheduler();
+//             vTaskDelay(pdMS_TO_TICKS(1000));
+//         }
+//     }
+// }
 
-void print_task(void *pvParameter)
-{
-    while (1)
-    {
-        edf_print_task_arrays();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
+// void print_task(void *pvParameter)
+// {
+//     while (1)
+//     {
+//         edf_print_task_arrays();
+//         vTaskDelay(pdMS_TO_TICKS(1000));
+//     }
+// }
 
 void app_main(void)
 {
