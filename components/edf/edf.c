1b70829f5074c1d16be01e8d26c1cfc8302863ab
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2025-04-16 16:59:15 +0200

    promjene

--- components/edf/edf.c
+++ components/edf/edf.c
@@ -8,81 +8,64 @@
 
 TaskHandle_t scheduler_handle;
 
-EDFtask tasksReady[MAX_TASKS];
-EDFtask tasksYield[MAX_TASKS];
-int numTasks = 0;
+EDFtask *tasksReady[MAX_TASKS] = {NULL}; //promjenio u kazaljke
+//EDFtask *tasksYield[MAX_TASKS]; //mislim da ovo polje nije potrebno radi funkcionalnosti, samo radi ispisa
+int numTasks = 0; //broj taskova u tasksReady
 
 // Testna funkcija koja printa ready i yield arrayeve
 void edf_print_task_arrays() 
 {
     printf("tasksReady: ");
     for (int i = 0; i < MAX_TASKS; i++) {
-        printf("%d ", (int) tasksReady[i].globalDeadline);
+        printf("%d ", (int) tasksReady[i]->globalDeadline);
     }
-    printf("\ntasksYield: ");
-    for (int i = 0; i < MAX_TASKS; i++) {
-        printf("%d ", (int) tasksYield[i].globalDeadline);
-    }
+    // printf("\ntasksYield: ");
+    // for (int i = 0; i < MAX_TASKS; i++) {
+    //     printf("%d ", (int) tasksYield[i]->globalDeadline);
+    // }
     printf("\n");
 }
 
 // miče task iz yield arraya i stavlja ga u ready array (array sortiran prema globalnom deadlineu)
 // ako je task predan funkciji novi ili ako je prvi u arrayu, probudi scheduler task
-int edf_ready_task(EDFtask task) 
+int edf_ready_task(EDFtask *task) 
 {
-    // if (tasksReady[MAX_TASKS-1].globalDeadline != 0) {
-    //     printf("Ready array is full.\n");
-    //     return 1;
-    // }
     int i;
-    int newTaskFlag = 0;
-    for (i = 0; i < numTasks; i++) {
-        if (tasksYield[i].handle == task.handle) {
-            tasksYield[i] = (EDFtask) {0};
-            break;
-        }
-    }
-    if (i == numTasks)
-        newTaskFlag = 1;
+    // int newTaskFlag = 0;
+    // for (i = 0; i < numTasks; i++) {
+    //     if (tasksYield[i]->handle == task->handle) {
+    //         tasksYield[i] = (EDFtask) {0};
+    //         break;
+    //     }
+    // }
+    // if (i == numTasks)
+    //     newTaskFlag = 1;
 
     for (i = 0; i < numTasks; i++) {
-        if (tasksReady[i].globalDeadline == 0 || task.globalDeadline < tasksReady[i].globalDeadline) {
+        if (task->globalDeadline < tasksReady[i]->globalDeadline) {
             break;
         }
     }
     for (int j = numTasks-1; j >= i; j--) {
-        if (tasksReady[j].globalDeadline == 0)
-            continue;
         tasksReady[j+1] = tasksReady[j];
     }
     tasksReady[i] = task;
+    numTasks += 1;
 
-    if ((newTaskFlag || i == 0) && scheduler_handle != 0)
-        xTaskNotifyGive(scheduler_handle);
+    xTaskNotifyGive(scheduler_handle);
+
     return 0;
 }
 
 // task se stavlja u yield array i budi scheduler task
-void edf_completed(EDFtask task) 
+void edf_completed(EDFtask *task) 
 {
-    // int i;
-    // for (i = 0; i < NUM_CORES; i++) {
-    //     if (tasksReady[i].handle == task.handle) {
-    //         tasksReady[i] = (EDFtask) {0};
+    // for (int j = 0; j < numTasks; j++) {
+    //     if (tasksYield[j]->handle == 0) {
+    //         tasksYield[j] = task;
     //         break;
     //     }
     // }
-    // for (int j = i+1; j < numTasks; j++) {
-    //     if (tasksReady[j].globalDeadline == 0)
-    //         break;
-    //     tasksReady[j-1] = tasksReady[j];
-    // }
-    for (int j = 0; j < numTasks; j++) {
-        if (tasksYield[j].handle == 0) {
-            tasksYield[j] = task;
-            break;
-        }
-    }
     xTaskNotifyGive(scheduler_handle);
 }
 
@@ -90,17 +73,18 @@ void edf_completed(EDFtask task)
 // edf_yield je macro za vTaskDelayUntil - zaustavlja task do kraja perioda
 void edf_task(void *pvParameter) 
 {
-    EDFtask task = *((EDFtask *) pvParameter);
+    EDFtask *task = *((EDFtask *) pvParameter);
 
+    TickType_t current_time = xTaskGetTickCount();
     while (1) {
-        TickType_t current_time = xTaskGetTickCount();
-        task.globalDeadline = current_time + pdMS_TO_TICKS(task.deadline);
+        task->globalDeadline = current_time + pdMS_TO_TICKS(task->deadline);
         edf_ready_task(task);
         // provjeriti return value
         uint32_t notif = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        task.task(task.params);
+        task->task(task->params);
         edf_completed(task);
-        edf_yield(&current_time, pdMS_TO_TICKS(task.period));
+        vTaskDelayUntil(&current_time, pdMS_TO_TICKS(task->period));
+        //current_time += pdMS_TO_TICKS(task->period) -> ovo radi prethodna funkcija!
     }
 }
 
@@ -109,59 +93,42 @@ TaskHandle_t edf_set(TaskFunction_t task, void *params, size_t param_size, int p
 {
     if (numTasks == MAX_TASKS) {
         printf("Can't add any more tasks.");
-        return 0;
-    }
-    void *param_copy = malloc(param_size);
-    if (param_copy == NULL) {
-        printf("Memory allocation failed.");
-        return 0;
+        return -1;
     }
 
-    // mozda bolje napraviti malloc prije samog poziva edf_set umjesto kopiranja? 
-    memcpy(param_copy, params, param_size);
-    EDFtask e;
-    e.period = period;
-    e.deadline = deadline;
-    e.task = task;
-    e.params = param_copy;
-    xTaskCreate(edf_task, "EDF task", 2048, (void *) &e, 5, &e.handle);
-    numTasks += 1;
-    return e.handle;
+    EDFtask *e = malloc(sizeof(EDFtask));
+    if (e == NULL) {
+        printf("Memory allocation failed.");
+        return -1;
+    }
+    e->period = period;
+    e->deadline = deadline;
+    e->task = task;
+    e->params = params;
+    xTaskCreate(edf_task, "EDF task", 2048, (void *) e, 5, &e->handle);
+    return e->handle;
 }
 
 // miče task iz ready arraya
-void edf_unready_task(EDFtask task) 
+void edf_unready_task(EDFtask *task) 
 {
-    int i;
-    for (i = 0; i < NUM_CORES; i++) {
-        if (tasksReady[i].handle == task.handle) {
-            tasksReady[i] = (EDFtask) {0};
-            break;
-        }
-    }
-    for (int j = i+1; j < numTasks+1; j++) {
+    //miče se ali trebala bi biti prva?
+    for (int j = 1; j < numTasks; j++) {
         tasksReady[j-1] = tasksReady[j];
-        if (tasksReady[j].globalDeadline == 0)
-            break;
     }
+    numTasks -= 1;
+
 }
 
 // scheduler - čeka da ga netko pozove pa budi task koji je prvi u ready arrayu i miče ga iz nje
 void edf_scheduler_task(void *pvParameter) 
 {
-    
-    // samo za jedan core trenutno
-    if (tasksReady[0].deadline != 0) {
-        xTaskNotifyGive(tasksReady[0].handle);
-        edf_unready_task(tasksReady[0]);
-    }
-
     while (1) 
     {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        if (tasksReady[0].deadline != 0) {
-            xTaskNotifyGive(tasksReady[0].handle);
+        if (tasksReady[0]->deadline != 0) {
             edf_unready_task(tasksReady[0]);
+            xTaskNotifyGive(tasksReady[0]->handle);
         }
     }
 }
--- main/main.c
+++ main/main.c
@@ -3,17 +3,6 @@
 #include "driver/gpio.h"
 #include "esp_system.h"
 
-// #define BUTTON_PIN GPIO_NUM_26
-
-// static uint32_t last_interrupt_time = 0;
-// 
-// void IRAM_ATTR isr_handler(void *arg) {
-//     uint32_t current_time = xTaskGetTickCountFromISR();
-//     if ((current_time - last_interrupt_time) > pdMS_TO_TICKS(200)) {
-//     }
-//     last_interrupt_time = current_time;
-// }
-
 void test(void *pvParameter) {
     int n = *((int *) pvParameter);
     // printf("START WORK\n");
@@ -22,39 +11,13 @@ void test(void *pvParameter) {
     // printf("END WORK\n");
 }
 
-// void button_task(void *pvParameter)
-// {
-//     while (1) 
-//     {
-//         if (gpio_get_level(BUTTON_PIN)) {
-//             edf_start_scheduler();
-//             vTaskDelay(pdMS_TO_TICKS(1000));
-//         }
-//     }
-// }
-
-// void print_task(void *pvParameter)
-// {
-//     while (1)
-//     {
-//         edf_print_task_arrays();
-//         vTaskDelay(pdMS_TO_TICKS(1000));
-//     }
-// }
+static int n = 3000;
+static int m = 1000;
 
 void app_main(void)
 {
-    // gpio_set_direction(BUTTON_PIN, GPIO_MODE_DEF_INPUT);
-    // xTaskCreate(button_task, "button", 2048, NULL, 10, NULL);
-    // xTaskCreate(print_task, "print", 2048, NULL, 10, NULL);
-    int n = 3000;
     edf_set(test, &n, sizeof(n), 10000, 7000);
-    n = 1000;
-    edf_set(test, &n, sizeof(n), 8000, 6000);
+    edf_set(test, &m, sizeof(m), 8000, 6000);
     vTaskDelay(1000);
     edf_start_scheduler();
-    // while (1) {
-    //     edf_print_task_arrays();
-    //     vTaskDelay(1000);
-    // }
 }
